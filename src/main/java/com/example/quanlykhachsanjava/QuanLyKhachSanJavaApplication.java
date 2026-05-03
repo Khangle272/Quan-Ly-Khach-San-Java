60380544767f0d53a7fbdbfd749b89cf9966afc6
Khangle272 <172981433+Khangle272@users.noreply.github.com>
2026-05-03 10:41:14 +0700

    init commit

---
+++ src/main/java/com/example/quanlykhachsanjava/QuanLyKhachSanJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlykhachsanjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyKhachSanJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyKhachSanJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quanlykhachsanjava/QuanLyKhachSanJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlykhachsanjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyKhachSanJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
